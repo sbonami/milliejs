4518965003be91cf2f6177fa397761fe2ed55c74
Scott BonAmi <1145657+sbonami@users.noreply.github.com>
2023-04-02 13:49:15 -0400

    chore: setup jest test coverage

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,16 @@ module.exports = {
 
   testRegex: "packages/(.*)/tests/(unit|integration)/(.*).(spec|test).ts$",
 
+  collectCoverage: true,
+  collectCoverageFrom: ["packages/*/src/**/*.{js,ts}"],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+
   resetMocks: true,
 }
