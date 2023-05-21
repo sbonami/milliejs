d7483af27bfe43858e5125e13dc33b674130aa0a
Scott BonAmi <1145657+sbonami@users.noreply.github.com>
2023-05-20 22:54:05 -0400

    fix: mock event functions for publisher

--- packages/milliejs/tests/unit/mocks/publisher.ts
+++ packages/milliejs/tests/unit/mocks/publisher.ts
@@ -1,7 +1,7 @@
+import { EventEmitter } from "node:events"
 import type { Resource } from "@milliejs/core"
 import type { PublisherActionInterface } from "@milliejs/store-base"
 import type { PublisherActionEventInterface } from "../../../src/store/events"
-import { makeMockSubscriber } from "./subscriber"
 
 export const makeMockPublisher = <R extends Resource>(
   partial?: Partial<PublisherActionInterface<R>>,
@@ -16,9 +16,15 @@ export const makeMockPublisher = <R extends Resource>(
 
 export const makeMockPublisherWithEvents = <R extends Resource>(
   partial?: Partial<PublisherActionEventInterface<R>>,
-): PublisherActionEventInterface<R> =>
-  ({
-    ...makeMockSubscriber(),
-    ...makeMockPublisher<R>(),
-    ...partial,
-  } as PublisherActionEventInterface<R>)
+): PublisherActionEventInterface<R> => {
+  let publisher = makeMockPublisher()
+
+  publisher = Object.getOwnPropertyNames(EventEmitter.prototype).reduce(
+    (_publisher, property) => {
+      return { ..._publisher, [property]: jest.fn() }
+    },
+    publisher,
+  )
+
+  return publisher as PublisherActionEventInterface<R>
+}
