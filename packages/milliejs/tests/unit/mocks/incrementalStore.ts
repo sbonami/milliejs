88dd2d20dbb16fb495efc8bbdc412591248bd3bd
Scott BonAmi <1145657+sbonami@users.noreply.github.com>
2023-05-20 22:53:26 -0400

    fix: generate mock for each instance

--- packages/milliejs/tests/unit/mocks/incrementalStore.ts
+++ packages/milliejs/tests/unit/mocks/incrementalStore.ts
@@ -3,14 +3,13 @@ import type { PublisherActionInterface } from "@milliejs/store-base"
 import type { StoreConstructorSourceOptions } from "../../../src/incremental"
 import type { PublisherActionEventInterface } from "../../../src/store/events"
 
-const { IncrementalStore: MockIncrementalStore } = jest.createMockFromModule<
-  typeof import("../../../src/incremental")
->("../../../src/incremental")
-
 export const makeMockIncrementalStore = <R extends Resource>(
   resource: R,
   replicaStore: PublisherActionEventInterface<R> | PublisherActionInterface<R>,
   sourceInterfaces?: StoreConstructorSourceOptions<R>,
 ) => {
+  const { IncrementalStore: MockIncrementalStore } = jest.createMockFromModule<
+    typeof import("../../../src/incremental")
+  >("../../../src/incremental")
   return new MockIncrementalStore(resource, replicaStore, sourceInterfaces)
 }
