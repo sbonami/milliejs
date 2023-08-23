9564105f5fff01db43ea5f33731263ea70f0a2c4
Scott BonAmi <1145657+sbonami@users.noreply.github.com>
2023-08-23 09:03:14 -0400

    ci(ci-lint-commit): warn for excessive commit message line length

--- commitlint.config.js
+++ commitlint.config.js
@@ -5,6 +5,12 @@ const Configuration = {
    * Referenced packages must be installed
    */
   extends: ["@commitlint/config-conventional"],
+  /*
+   * Any rules defined here will override rules from @commitlint/config-conventional
+   */
+  rules: {
+    "body-max-line-length": [1, "always", 100],
+  },
 }
 
 module.exports = Configuration
