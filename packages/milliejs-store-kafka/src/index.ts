58c36100a8776e5c2daf02ddc2fbddc8a6fb18ec
Scott BonAmi <1145657+sbonami@users.noreply.github.com>
2023-09-30 16:08:38 -0400

    feat(store-kafka): add subscription functionality

--- packages/milliejs-store-kafka/src/index.ts
+++ packages/milliejs-store-kafka/src/index.ts
@@ -1,23 +1,76 @@
 import EventEmitter from "node:events"
 import {
+  Consumer,
+  ConsumerConfig,
+  ConsumerSubscribeTopics,
+  EachMessagePayload,
   Kafka,
-  KafkaConfig
+  KafkaConfig,
 } from "kafkajs"
 import type {
+  Entity,
   Resource,
+  SubscriberActionEventKeys,
   SubscriberActionInterface,
 } from "@milliejs/store-base"
+
+type ParsedMessage<R extends Resource> = {
+  eventName: SubscriberActionEventKeys<R>
+  entity: Entity<R>
+}
+
+export type MessageParser<R extends Resource> = (
+  message: EachMessagePayload,
+) => ParsedMessage<R>
+
+export type KafkaStoreConfig = KafkaConfig & { consumer: ConsumerConfig }
+
 export class KafkaSubscriber<R extends Resource = Resource<unknown>>
   extends EventEmitter
   implements SubscriberActionInterface
 {
   private client: Kafka
+  private consumer: Consumer
 
   constructor(
-    clientOptions: KafkaConfig
+    clientOptions: KafkaStoreConfig,
+    private readonly topics: ConsumerSubscribeTopics[],
+    readonly parser: MessageParser<R>,
   ) {
+    const { consumer: consumerConfig, ..._config } = clientOptions
     super()
-    this.client = new Kafka(clientOptions)
+    this.client = new Kafka(_config)
+
+    this.consumer = this.client.consumer(consumerConfig)
+  }
+
+  async connect() {
+    await this.consumer.connect()
+    await Promise.all(
+      this.topics.map((topic) => this.consumer.subscribe(topic)),
+    )
+    await this.consumer.run({
+      eachMessage: async (messagePayload: EachMessagePayload) => {
+        const { topic, partition, message } = messagePayload
+        const prefix = `${topic}[${partition} | ${message.offset}] / ${message.timestamp}`
+        console.log(`- ${prefix} ${message.key}#${message.value}`)
+
+        this.messageListener(messagePayload)
+      },
+    })
+  }
+
+  async disconnect() {
+    await this.consumer.disconnect()
+  }
+
+  private messageListener(message: EachMessagePayload) {
+    try {
+      const { eventName, entity } = this.parser(message)
+      this.emit(eventName, entity)
+    } catch (error) {
+      this.emit("error", error)
+    }
   }
 }
 
--- packages/milliejs-store-kafka/tests/unit/index.test.ts
+++ packages/milliejs-store-kafka/tests/unit/index.test.ts
@@ -1,16 +1,50 @@
-import { Kafka, KafkaConfig } from "kafkajs"
-import KafkaStore from "../../src"
+import { makeMockEntity } from "@milliejs/jest-utils"
+import {
+  Consumer,
+  EachMessageHandler,
+  EachMessagePayload,
+  Kafka,
+  KafkaConfig,
+} from "kafkajs"
+import KafkaStore, { KafkaStoreConfig } from "../../src"
+import { LifecycleEvents } from "@milliejs/store-base"
 
 jest.mock("kafkajs")
 const MockKafka = jest.mocked(Kafka)
+const mockConsumer: Consumer = {
+  connect: jest.fn(),
+  disconnect: jest.fn(),
+  subscribe: jest.fn(),
+  stop: jest.fn(),
+  seek: jest.fn(),
+  run: jest.fn(),
+  commitOffsets: jest.fn(),
+  describeGroup: jest.fn(),
+  pause: jest.fn(),
+  paused: jest.fn(),
+  resume: jest.fn(),
+  on: jest.fn(),
+  logger: jest.fn(),
+  events: jest.fn(),
+} as unknown as jest.MockedObject<Consumer>
 
 describe("@millie/store-kafka", () => {
   describe("new KafkaStore()", () => {
+    const clientOptions: KafkaConfig = {
+      brokers: ["localhost:9092"],
+    }
+    const consumerOptions: KafkaStoreConfig["consumer"] = {
+      groupId: "testGroupId",
+    }
+    const topics = [{ topics: ["test-topic"] }]
+    const parser = jest.fn()
+
     it("returns a new instance of the KafkaStore", () => {
-      const options: KafkaConfig = {
-        brokers: ["localhost:9092"],
-      }
-      const store = new KafkaStore(options)
+      const store = new KafkaStore(
+        { ...clientOptions, consumer: consumerOptions },
+        topics,
+        parser,
+      )
       expect(store).toBeInstanceOf(KafkaStore)
     })
 
@@ -19,14 +53,260 @@ describe("@millie/store-kafka", () => {
         brokers: ["localhost:9092"],
       })
       MockKafka.mockReturnValue(client)
+      const consumerClient = jest.fn()
+      jest
+        .spyOn(Kafka.prototype, "consumer")
+        .mockReturnValue(consumerClient as unknown as Consumer)
 
-      const options: KafkaConfig = {
-        brokers: ["localhost:9092"],
-      }
-      const store = new KafkaStore(options)
-
-      expect(MockKafka).toHaveBeenCalledWith(options)
+      const store = new KafkaStore(
+        { ...clientOptions, consumer: consumerOptions },
+        topics,
+        parser,
+      )
+      expect(MockKafka).toHaveBeenCalledWith(clientOptions)
       expect(store["client"]).toBe(client)
+      expect(store["topics"]).toBe(topics)
+      expect(store["parser"]).toBe(parser)
+    })
+
+    it("initializes a Kafka consumer with the provided consumer options", () => {
+      const client = new Kafka({
+        brokers: ["localhost:9092"],
+      })
+      MockKafka.mockReturnValue(client)
+      const consumerClient = jest.fn()
+      jest
+        .spyOn(Kafka.prototype, "consumer")
+        .mockReturnValue(consumerClient as unknown as Consumer)
+
+      const store = new KafkaStore(
+        { ...clientOptions, consumer: consumerOptions },
+        topics,
+        parser,
+      )
+      expect(client.consumer).toHaveBeenCalledWith(consumerOptions)
+      expect(store["consumer"]).toBe(consumerClient)
+    })
+  })
+
+  describe("#connect()", () => {
+    it("connects the consumer", async () => {
+      const options: KafkaStoreConfig = {
+        brokers: ["localhost:9092"],
+        consumer: {
+          groupId: "testGroupId",
+        },
+      }
+      const topics = [{ topics: ["test-topic"] }]
+      const parser = jest.fn()
+
+      const client = new Kafka({
+        brokers: ["localhost:9092"],
+      })
+      MockKafka.mockReturnValue(client)
+      jest.spyOn(MockKafka.prototype, "consumer").mockReturnValue(mockConsumer)
+
+      const store = new KafkaStore(options, topics, parser)
+
+      await store.connect()
+      expect(mockConsumer.connect).toHaveBeenCalled()
+    })
+
+    it("subscribes each of the topics with the consumer", async () => {
+      const options: KafkaStoreConfig = {
+        brokers: ["localhost:9092"],
+        consumer: {
+          groupId: "testGroupId",
+        },
+      }
+      const topics = [{ topics: ["test-topic"] }]
+      const parser = jest.fn()
+
+      const client = new Kafka({
+        brokers: ["localhost:9092"],
+      })
+      MockKafka.mockReturnValue(client)
+      jest.spyOn(MockKafka.prototype, "consumer").mockReturnValue(mockConsumer)
+
+      const store = new KafkaStore(options, topics, parser)
+
+      await store.connect()
+      expect(mockConsumer.subscribe).toHaveBeenCalledWith(topics[0])
+    })
+
+    it("registers a message processor with the consumer", async () => {
+      const options: KafkaStoreConfig = {
+        brokers: ["localhost:9092"],
+        consumer: {
+          groupId: "testGroupId",
+        },
+      }
+      const topics = [{ topics: ["test-topic"] }]
+      const parser = jest.fn()
+
+      const client = new Kafka({
+        brokers: ["localhost:9092"],
+      })
+      MockKafka.mockReturnValue(client)
+      jest.spyOn(MockKafka.prototype, "consumer").mockReturnValue(mockConsumer)
+
+      const store = new KafkaStore(options, topics, parser)
+
+      await store.connect()
+      expect(mockConsumer.run).toHaveBeenCalledWith(
+        expect.objectContaining({
+          eachMessage: expect.any(Function),
+        }),
+      )
+    })
+  })
+
+  describe("#disconnect()", () => {
+    it("disconnect the consumer", async () => {
+      const options: KafkaStoreConfig = {
+        brokers: ["localhost:9092"],
+        consumer: {
+          groupId: "testGroupId",
+        },
+      }
+      const topics = [{ topics: ["test-topic"] }]
+      const parser = jest.fn()
+
+      const client = new Kafka({
+        brokers: ["localhost:9092"],
+      })
+      MockKafka.mockReturnValue(client)
+      jest.spyOn(MockKafka.prototype, "consumer").mockReturnValue(mockConsumer)
+
+      const store = new KafkaStore(options, topics, parser)
+
+      await store.disconnect()
+      expect(mockConsumer.disconnect).toHaveBeenCalled()
+    })
+  })
+
+  describe("[message event]", () => {
+    const options: KafkaStoreConfig = {
+      brokers: ["localhost:9092"],
+      consumer: {
+        groupId: "testGroupId",
+      },
+    }
+    const topics = [{ topics: ["test-topic"] }]
+    let store: KafkaStore
+    let eachMessageCallback: EachMessageHandler
+    const parser = jest.fn()
+
+    beforeEach(async () => {
+      const client = new Kafka({
+        brokers: ["localhost:9092"],
+      })
+      MockKafka.mockReturnValue(client)
+      jest.spyOn(MockKafka.prototype, "consumer").mockReturnValue(mockConsumer)
+      jest.spyOn(mockConsumer, "run").mockImplementation((config) => {
+        eachMessageCallback = config!.eachMessage!
+        return Promise.resolve()
+      })
+
+      store = new KafkaStore(options, topics, parser)
+      await store.connect()
+    })
+
+    describe("when the Kafka store receives a message", () => {
+      it("parses the message using the constructor's parser", async () => {
+        const mockEntity = makeMockEntity()
+        parser.mockReturnValue({
+          eventName: LifecycleEvents.Save,
+          entity: mockEntity,
+        })
+
+        const message: EachMessagePayload = {
+          topic: "test-topic",
+          partition: 0,
+          message: {
+            key: null,
+            value: null,
+            timestamp: Date.now().toString(),
+            attributes: 0,
+            offset: "",
+            size: 0,
+          },
+          heartbeat: jest.fn(),
+          pause: jest.fn(),
+        }
+
+        eachMessageCallback(message)
+        expect(parser).toHaveBeenCalledWith(message)
+      })
+
+      describe("when the parsing is successful", () => {
+        it("emits an event with the name from the parsed message", (done) => {
+          const mockEntity = makeMockEntity()
+          parser.mockReturnValue({
+            eventName: LifecycleEvents.Save,
+            entity: mockEntity,
+          })
+
+          store.once(LifecycleEvents.Save, (entity) => {
+            try {
+              expect(entity).toBe(mockEntity)
+              done()
+            } catch (error) {
+              done(error)
+            }
+          })
+
+          const message: EachMessagePayload = {
+            topic: "test-topic",
+            partition: 0,
+            message: {
+              key: null,
+              value: null,
+              timestamp: Date.now().toString(),
+              attributes: 0,
+              offset: "",
+              size: 0,
+            },
+            heartbeat: jest.fn(),
+            pause: jest.fn(),
+          }
+          eachMessageCallback(message)
+        })
+      })
+
+      describe("when the processing fails", () => {
+        it("re-emits the error from the store", (done) => {
+          const mockError = new Error("Mock Error")
+          parser.mockImplementation(() => {
+            throw mockError
+          })
+
+          store.once("error", (parsingError) => {
+            try {
+              expect(parsingError).toBe(mockError)
+              done()
+            } catch (error) {
+              done(error)
+            }
+          })
+
+          const message: EachMessagePayload = {
+            topic: "test-topic",
+            partition: 0,
+            message: {
+              key: null,
+              value: null,
+              timestamp: Date.now().toString(),
+              attributes: 0,
+              offset: "",
+              size: 0,
+            },
+            heartbeat: jest.fn(),
+            pause: jest.fn(),
+          }
+          eachMessageCallback(message)
+        })
+      })
     })
   })
 })
